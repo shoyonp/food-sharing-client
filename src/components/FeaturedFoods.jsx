e2665a9cc8ce7b99122365a896dc5c5dfbf15520
shoyonp <shoyonkumar143@gmail.com>
2024-12-25 06:01:07 +0600

    sorting by expired date fuctionality implement done

--- src/components/FeaturedFoods.jsx
+++ src/components/FeaturedFoods.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import FeaturedFoodCard from "./FeaturedFoodCard";
+import { Link } from "react-router-dom";
 
 const FeaturedFoods = () => {
   const [foods, setFoods] = useState([]);
@@ -13,10 +14,11 @@ const FeaturedFoods = () => {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-5">
-        {foods.map((food) => (
+        {foods.slice(0, 6).map((food) => (
           <FeaturedFoodCard key={food._id} food={food}></FeaturedFoodCard>
         ))}
       </div>
+      <Link to="/availableFood"><button className="btn btn-warning">Show All</button></Link>
     </div>
   );
 };
--- src/pages/AvailableFoods.jsx
+++ src/pages/AvailableFoods.jsx
@@ -1,30 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import FeaturedFoodCard from '../components/FeaturedFoodCard';
+import React, { useEffect, useState } from "react";
+import FeaturedFoodCard from "../components/FeaturedFoodCard";
 
 const AvailableFoods = () => {
+  const [foods, setFoods] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState('');
+  // const handleSearch =(e)=>{
+  //     const search = e.target.value
+  //     console.log(search);
+  // }
 
-    const [foods, setFoods] = useState([]);
-
-    const handleSearch =(e)=>{
-        const search = e.target.value
-        console.log(search);
-    }
-    
-      useEffect(() => {
-        fetch("http://localhost:5000/foods")
-          .then((res) => res.json())
-          .then((data) => setFoods(data));
-      }, []);
-    return (
+  useEffect(() => {
+    fetch(`http://localhost:5000/foods?search=${search}&sort=${sort}`)
+      .then((res) => res.json())
+      .then((data) => setFoods(data));
+  }, [search,sort]);
+  return (
+    <div>
+      <div className="flex mx-auto justify-center">
         <div>
-            <input className='border border-red-400 w-1/3 ms-auto' onChange={handleSearch} type="text" name="" id="" />
+          <input
+            className="border border-red-400 w-full mx-auto"
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            name=""
+            id=""
+          />
+        </div>
+        <div>
+          <select name="" id="" onChange={(e) => setSort(e.target.value)}>
+            <option value="">Sort By Expired Date</option>
+            <option value="dsc">Descending Order</option>
+            <option value="asc">Ascending Order</option>
+          </select>
+        </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-5">
         {foods.map((food) => (
           <FeaturedFoodCard key={food._id} food={food}></FeaturedFoodCard>
         ))}
       </div>
     </div>
-    );
+  );
 };
 
 export default AvailableFoods;

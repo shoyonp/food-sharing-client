040f4cb1737d766890dbca472a8adb3264442bff
shoyonp <shoyonkumar143@gmail.com>
2024-12-25 21:50:01 +0600

    layout change and requested data not show to frontend functionality added

--- src/components/FeaturedFoods.jsx
+++ src/components/FeaturedFoods.jsx
@@ -2,27 +2,32 @@ import FeaturedFoodCard from "./FeaturedFoodCard";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const FeaturedFoods = () => {
-//   const [foods, setFoods] = useState([]);
+  //   const [foods, setFoods] = useState([]);
+  const [availableData, setAvailableData] = useState([]);
+  //   useEffect(() => {
+  //     fetchAllfoods();
+  //   }, []);
 
-//   useEffect(() => {
-//     fetchAllfoods();
-//   }, []);
+  //   const fetchAllfoods = async () => {
+  //     const { data } = await axios.get("http://localhost:5000/foods");
+  //     setFoods(data);
+  //   };
 
-//   const fetchAllfoods = async () => {
-//     const { data } = await axios.get("http://localhost:5000/foods");
-//     setFoods(data);
-//   };
-
-  const { data:foods, isLoading } = useQuery({
+  // us
+  const { data: foods, isLoading } = useQuery({
     queryKey: ["foods"],
     queryFn: async () => {
       const { data } = await axios.get("http://localhost:5000/foods");
       return data;
     },
   });
-
+  useEffect(() => {
+    const filterFoods = foods.filter((food) => food.foodStatus === "available");
+    setAvailableData(filterFoods);
+  }, [foods]);
   if (isLoading) {
     return (
       <span className="loading loading-dots loading-lg mx-auto flex justify-center  min-h-screen"></span>
@@ -31,9 +36,12 @@ const FeaturedFoods = () => {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-5">
-        {foods.slice(0, 6).map((food) => (
-          <FeaturedFoodCard key={food._id} food={food}></FeaturedFoodCard>
-        ))}
+        {availableData
+          ?.slice(0, 6)
+          .reverse()
+          ?.map((food) => (
+            <FeaturedFoodCard key={food._id} food={food}></FeaturedFoodCard>
+          ))}
       </div>
       <Link to="/availableFood">
         <button className="btn btn-warning">Show All</button>
--- src/pages/AvailableFoods.jsx
+++ src/pages/AvailableFoods.jsx
@@ -4,17 +4,23 @@ import FeaturedFoodCard from "../components/FeaturedFoodCard";
 const AvailableFoods = () => {
   const [foods, setFoods] = useState([]);
   const [search, setSearch] = useState("");
-  const [sort, setSort] = useState('');
-  // const handleSearch =(e)=>{
-  //     const search = e.target.value
-  //     console.log(search);
-  // }
+  const [sort, setSort] = useState("");
+  const [layout, setLayout] = useState(true);
+  const [availableData, setAvailableData] = useState([]);
+  //   console.log(foods);
+  const toggleLayout = () => {
+    setLayout(!layout);
+  };
 
   useEffect(() => {
     fetch(`http://localhost:5000/foods?search=${search}&sort=${sort}`)
       .then((res) => res.json())
       .then((data) => setFoods(data));
-  }, [search,sort]);
+  }, [search, sort]);
+  useEffect(() => {
+    const filterFoods = foods.filter((food) => food.foodStatus === "available");
+    setAvailableData(filterFoods);
+  }, [foods]);
   return (
     <div>
       <div className="flex mx-auto justify-center">
@@ -34,9 +40,22 @@ const AvailableFoods = () => {
             <option value="asc">Ascending Order</option>
           </select>
         </div>
+        <div>
+          {" "}
+          <button
+            onClick={toggleLayout}
+            className="mb-4 px-4 py-2 bg-blue-500 text-white rounded"
+          >
+            Change layout
+          </button>
+        </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-5">
-        {foods.map((food) => (
+      <div
+        className={` grid grid-cols-1 md:grid-cols-2 mx-auto gap-5 ${
+          layout ? "lg:grid-cols-3" : "lg:grid-cols-2"
+        } `}
+      >
+        {[...availableData]?.reverse()?.map((food) => (
           <FeaturedFoodCard key={food._id} food={food}></FeaturedFoodCard>
         ))}
       </div>

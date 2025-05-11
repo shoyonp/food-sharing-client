6a15112f3b22af7354a472135f320e6bc7b47f26
shoyonp <shoyonkumar143@gmail.com>
2025-05-11 13:50:26 +0600

    animation added in feturedfod section

--- src/components/FeaturedFoods.jsx
+++ src/components/FeaturedFoods.jsx
@@ -2,6 +2,7 @@ import FeaturedFoodCard from "./FeaturedFoodCard";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 const FeaturedFoods = () => {
@@ -44,18 +45,29 @@ const FeaturedFoods = () => {
         {availableData
           ?.slice(0, 6)
           .reverse()
-          ?.map((food) => (
-            <FeaturedFoodCard key={food._id} food={food}></FeaturedFoodCard>
+          ?.map((food, index) => (
+            <motion.div
+              key={food._id}
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+             
+            >
+              <FeaturedFoodCard food={food} />
+            </motion.div>
           ))}
       </div>
+
       <div className="flex justify-center mt-7">
         <Link to="/availableFood">
-          <button
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
             className="btn px-4 py-2 bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 text-white 
-        hover:from-gray-500 hover:via-gray-600 hover:to-gray-700 transform transition duration-300 hover:scale-110"
+            hover:from-gray-500 hover:via-gray-600 hover:to-gray-700 transform transition duration-300"
           >
             Show All
-          </button>
+          </motion.button>
         </Link>
       </div>
     </>
